cb83d7cf37fafba028aebde0ca11faf5c630e05f
Lluis Adan <lluis.adan@gmail.com>
2024-04-24 12:46:21 +0200

    Feat: Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Link to="/">
+        <Navbar />
+      </Link>
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/:countryId" element={<CountryDetailsPage/>}></Route>
+      </Routes>
+      
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,28 @@
-function HomePage() {}
+import { Link } from 'react-router-dom';
+import dataJson from '../../data/response.json';
+
+function HomePage() {
+  const countries = dataJson;
+
+  return (
+    <>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      <div>
+        <ul className='list-group'>
+          {countries.map(country => (
+            <Link to={`/${country._id}`} key={country._id}>
+              <li className="list-group-item d-flex justify-content-center">
+                <img src={`https://flagcdn.com/${country.alpha2Code.toLowerCase()}.svg`} alt={country.name.common} width="3%" height="3%" ></img>
+                <p className="mx-2" style={{ fontSize: "20px", fontWeight: "bold" }}>{country.name.common}</p>
+              </li>
+            </Link>
+
+          ))}
+        </ul>
+      </div>
+    </>
+
+  )
+}
 
 export default HomePage;
